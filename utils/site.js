6ce686bcc906560af6548e7ba9c3fb6dd7b971f3
codesktop <codesktop@foxmail.com>
2021-03-21 03:20:29 +0800

    init commit

--- utils/site.js
+++ utils/site.js
@@ -2,7 +2,7 @@ export const paths = [
   { title: 'Daum Cafe', slug: 'daum-cafe' },
   { title: 'First Week', slug: 'first-week' },
   { title: 'As You Wish', slug: 'melon-daily' },
-  { title: 'Youtube Official and FanCam', slug: 'youtube-daily' }
+  { title: 'YouTube Official and FanCam', slug: 'youtube-daily' }
 ]
 
 export function page(slug) {
